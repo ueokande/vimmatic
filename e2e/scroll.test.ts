17ec8bc052cd15df243a9153cdbbd18713d3eed0
Shin'ya Ueoka <ueokande@i-beam.org>
2023-03-03 14:36:45 +0000

    use eventually equal for scroll test

--- e2e/scroll.test.ts
+++ e2e/scroll.test.ts
@@ -15,8 +15,7 @@ test("scrolls up by j", async ({ page }) => {
   await page.goto(server.url());
   await page.keyboard.press("j");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  expect(scrollY).toBe(64);
+  await expect.poll(() => page.evaluate(() => window.pageYOffset)).toBe(64);
 });
 
 test("scrolls down by k", async ({ page }) => {
@@ -24,8 +23,7 @@ test("scrolls down by k", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(0, 100));
   await page.keyboard.press("k");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  expect(scrollY).toBe(36);
+  await expect.poll(() => page.evaluate(() => window.pageYOffset)).toBe(36);
 });
 
 test("scrolls left by h", async ({ page }) => {
@@ -33,8 +31,7 @@ test("scrolls left by h", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(100, 0));
   await page.keyboard.press("h");
 
-  const scrollX = await page.evaluate(() => window.pageXOffset);
-  expect(scrollX).toBe(36);
+  await expect.poll(() => page.evaluate(() => window.pageXOffset)).toBe(36);
 });
 
 test("scrolls left by l", async ({ page }) => {
@@ -42,8 +39,7 @@ test("scrolls left by l", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(100, 0));
   await page.keyboard.press("l");
 
-  const scrollX = await page.evaluate(() => window.pageXOffset);
-  expect(scrollX).toBe(164);
+  await expect.poll(() => page.evaluate(() => window.pageXOffset)).toBe(164);
 });
 
 test("scrolls top by gg", async ({ page }) => {
@@ -52,16 +48,16 @@ test("scrolls top by gg", async ({ page }) => {
   await page.keyboard.press("g");
   await page.keyboard.press("g");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  expect(scrollY).toBe(0);
+  await expect.poll(() => page.evaluate(() => window.pageYOffset)).toBe(0);
 });
 
 test("scrolls bottom by G", async ({ page }) => {
   await page.goto(server.url());
   await page.keyboard.press("Shift+G");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  expect(scrollY).toBeGreaterThan(5000);
+  await expect
+    .poll(() => page.evaluate(() => window.pageYOffset))
+    .toBeGreaterThan(5000);
 });
 
 test("scrolls bottom by 0", async ({ page }) => {
@@ -69,8 +65,7 @@ test("scrolls bottom by 0", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(100, 100));
   await page.keyboard.press("0");
 
-  const scrollX = await page.evaluate(() => window.pageXOffset);
-  expect(scrollX).toBe(0);
+  await expect.poll(() => page.evaluate(() => window.pageXOffset)).toBe(0);
 });
 
 test("scrolls bottom by $", async ({ page }) => {
@@ -78,8 +73,9 @@ test("scrolls bottom by $", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(100, 100));
   await page.keyboard.press("$");
 
-  const scrollX = await page.evaluate(() => window.pageXOffset);
-  expect(scrollX).toBeGreaterThan(5000);
+  await expect
+    .poll(() => page.evaluate(() => window.pageXOffset))
+    .toBeGreaterThanOrEqual(5000);
 });
 
 test("scrolls bottom by <C-U>", async ({ page }) => {
@@ -87,12 +83,15 @@ test("scrolls bottom by <C-U>", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(0, 1000));
   await page.keyboard.press("Control+u");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  const pageHeight = await page.evaluate(
-    () => window.document.documentElement.clientHeight
-  );
-
-  expect(Math.abs(scrollY - (1000 - pageHeight / 2))).toBeLessThan(5);
+  await expect
+    .poll(async () => {
+      const scrollY = await page.evaluate(() => window.pageYOffset);
+      const pageHeight = await page.evaluate(
+        () => window.document.documentElement.clientHeight
+      );
+      return Math.abs(scrollY - (1000 - pageHeight / 2));
+    })
+    .toBeLessThan(5);
 });
 
 test("scrolls bottom by <C-D>", async ({ page }) => {
@@ -100,12 +99,15 @@ test("scrolls bottom by <C-D>", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(0, 1000));
   await page.keyboard.press("Control+d");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  const pageHeight = await page.evaluate(
-    () => window.document.documentElement.clientHeight
-  );
-
-  expect(Math.abs(scrollY - (1000 + pageHeight / 2))).toBeLessThan(5);
+  await expect
+    .poll(async () => {
+      const scrollY = await page.evaluate(() => window.pageYOffset);
+      const pageHeight = await page.evaluate(
+        () => window.document.documentElement.clientHeight
+      );
+      return Math.abs(scrollY - (1000 + pageHeight / 2));
+    })
+    .toBeLessThan(5);
 });
 
 test("scrolls bottom by <C-B>", async ({ page }) => {
@@ -113,12 +115,15 @@ test("scrolls bottom by <C-B>", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(0, 1000));
   await page.keyboard.press("Control+b");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  const pageHeight = await page.evaluate(
-    () => window.document.documentElement.clientHeight
-  );
-
-  expect(Math.abs(scrollY - (1000 - pageHeight))).toBeLessThan(5);
+  await expect
+    .poll(async () => {
+      const scrollY = await page.evaluate(() => window.pageYOffset);
+      const pageHeight = await page.evaluate(
+        () => window.document.documentElement.clientHeight
+      );
+      return Math.abs(scrollY - (1000 - pageHeight));
+    })
+    .toBeLessThan(5);
 });
 
 test("scrolls bottom by <C-F>", async ({ page }) => {
@@ -126,10 +131,13 @@ test("scrolls bottom by <C-F>", async ({ page }) => {
   await page.evaluate(() => window.scrollBy(0, 1000));
   await page.keyboard.press("Control+f");
 
-  const scrollY = await page.evaluate(() => window.pageYOffset);
-  const pageHeight = await page.evaluate(
-    () => window.document.documentElement.clientHeight
-  );
-
-  expect(Math.abs(scrollY - (1000 + pageHeight))).toBeLessThan(5);
+  await expect
+    .poll(async () => {
+      const scrollY = await page.evaluate(() => window.pageYOffset);
+      const pageHeight = await page.evaluate(
+        () => window.document.documentElement.clientHeight
+      );
+      return Math.abs(scrollY - (1000 + pageHeight));
+    })
+    .toBeLessThan(5);
 });
