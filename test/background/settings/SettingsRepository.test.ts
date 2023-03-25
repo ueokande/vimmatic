ueokande/vimmatic

commit ffc69a635580037a22df4686dcccae943cd98a9c

fix: persist worker script state (#96)

src/background/di.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 
 import { Container } from "inversify";
-import { LastSelectedTabImpl } from "./tabs/LastSelectedTab";
+import { LastSelectedTabRepositoryImpl } from "./repositories/LastSelectedTabRepository";
 import { NotifierImpl } from "./presenters/Notifier";
 import { ContentMessageClientImpl } from "./clients/ContentMessageClient";
 import { NavigateClientImpl } from "./clients/NavigateClient";
@@ -18,7 +18,7 @@ import {
 } from "./repositories/ClipboardRepository";
 import { PropertySettingsImpl } from "./settings/PropertySettings";
 import { SearchEngineSettingsImpl } from "./settings/SearchEngineSettings";
-import { TransientSettingsRepotiory } from "./settings/SettingsRepository";
+import { SettingsRepositoryImpl } from "./settings/SettingsRepository";
 import { KeyCaptureClientImpl } from "./clients/KeyCaptureClient";
 import { MarkRepositoryImpl } from "./repositories/MarkRepository";
 import { MarkModeRepositoryImpl } from "./repositories/MarkModeRepository";
@@ -35,7 +35,7 @@ import { OperatorRegistoryFactory } from "./operators";
 
 const container = new Container({ autoBindInjectable: true });
 
-container.bind("LastSelectedTab").to(LastSelectedTabImpl);
+container.bind("LastSelectedTabRepository").to(LastSelectedTabRepositoryImpl);
 container.bind("Notifier").to(NotifierImpl);
 container.bind("BrowserSettingRepository").to(BrowserSettingRepositoryImpl);
 container.bind("RepeatRepository").to(RepeatRepositoryImpl);
@@ -63,10 +63,7 @@ container.bind("TopFrameClient").to(TopFrameClientImpl);
 container.bind("AddonEnabledRepository").to(AddonEnabledRepositoryImpl);
 container.bind("AddonEnabledClient").to(AddonEnabledClientImpl);
 container.bind("ToolbarPresenter").to(ToolbarPresenterImpl);
-container
-  .bind("SettingsRepository")
-  .to(TransientSettingsRepotiory)
-  .inSingletonScope();
+container.bind("SettingsRepository").to(SettingsRepositoryImpl);
 container
   .bind("PropertyRegistry")
   .toConstantValue(new PropertyRegistryFactry().create());

src/background/settings/SettingsRepository.ts
@@ -1,8 +1,6 @@
-import { injectable, inject } from "inversify";
-import MemoryStorage from "../db/MemoryStorage";
-import { defaultSettings, serialize, deserialize } from "../../settings";
+import { injectable } from "inversify";
+import { defaultSettings, deserialize } from "../../settings";
 import Settings from "../../shared/Settings";
-import { SerializedSettings } from "../../settings/schema";
 
 type OnChangeListener = (value: Settings) => unknown;
 
@@ -14,31 +12,8 @@ export default interface SettingsRepository {
   onChanged(f: OnChangeListener): void;
 }
 
-class SettingsCache {
-  private readonly cache = new MemoryStorage<SerializedSettings | null>(
-    SettingsCache.name,
-    null
-  );
-
-  load(): Settings | null {
-    const cache = this.cache.get();
-    if (cache === null) {
-      return null;
-    }
-    return deserialize(cache);
-  }
-
-  save(value: Settings): void {
-    this.cache.set(serialize(value));
-  }
-
-  invalidate() {
-    this.cache.set(null);
-  }
-}
-
 @injectable()
-export class PersistentSettingsRepository implements SettingsRepository {
+export class SettingsRepositoryImpl implements SettingsRepository {
   async load(): Promise<Settings> {
     const { settings } = await chrome.storage.sync.get("settings");
     if (!settings) {
@@ -79,48 +54,5 @@ export class PersistentSettingsRepository implements SettingsRepository {
 
       f(settings);
     });
-  }
-}
-
-@injectable()
-export class TransientSettingsRepotiory implements SettingsRepository {
-  private readonly cache = new SettingsCache();
-
-  private readonly onChangeListeners: Array<OnChangeListener> = [];
-
-  constructor(
-    @inject(PersistentSettingsRepository)
-    private readonly origin: SettingsRepository
-  ) {
-    this.origin.onChanged(() => {
-      this.invalidate();
-    });
-  }
-
-  async load(): Promise<Settings> {
-    const cache = this.cache.load();
-    if (cache) {
-      return cache;
-    }
-
-    const origin = await this.origin.load();
-    this.save(origin);
-    return origin;
-  }
-
-  save(value: Settings): Promise<void> {
-    this.cache.save(value);
-    this.onChangeListeners.forEach((listener) => {
-      listener(value);
-    });
-    return Promise.resolve();
-  }
-
-  onChanged(f: OnChangeListener): void {
-    this.onChangeListeners.push(f);
-  }
-
-  private async invalidate(): Promise<void> {
-    this.cache.invalidate();
   }
 }

src/background/Application.ts
@@ -7,6 +7,7 @@ import ReadyFrameRepository from "./repositories/ReadyFrameRepository";
 import SettingsEventUseCase from "./usecases/SettingsEventUseCase";
 import FrameClient from "./clients/FrameClient";
 import AddonEnabledEventUseCase from "./usecases/AddonEnabledEventUseCase";
+import LastSelectedTabRepository from "./repositories/LastSelectedTabRepository";
 
 @injectable()
 export default class Application {
@@ -19,6 +20,8 @@ export default class Application {
     private readonly findRepository: FindRepositoryImpl,
     @inject("ReadyFrameRepository")
     private readonly frameRepository: ReadyFrameRepository,
+    @inject("LastSelectedTabRepository")
+    private readonly lastSelectedTabRepository: LastSelectedTabRepository,
     @inject(SettingsEventUseCase)
     private readonly settingsEventUseCase: SettingsEventUseCase,
     @inject("FrameClient")
@@ -41,35 +44,41 @@ export default class Application {
         this.findRepository.deleteLocalState(tabId);
       }
     });
+    chrome.runtime.onStartup.addListener(() => {
+      browser.storage.local.clear();
+    });
     chrome.runtime.onInstalled.addListener((details) => {
       if (details.reason !== "install" && details.reason !== "update") {
         return;
       }
       this.versionUseCase.notify();
     });
+    chrome.tabs.onActivated.addListener(({ tabId }) => {
+      this.lastSelectedTabRepository.setCurrentTabId(tabId);
+    });
 
     this.backgroundMessageListener.listen();
     this.findPortListener.run();
   }
 
-  private onFindPortConnect(port: chrome.runtime.Port) {
+  private async onFindPortConnect(port: chrome.runtime.Port) {
     const tabId = port.sender?.tab?.id;
     const frameId = port.sender?.frameId;
     if (typeof tabId === "undefined" || typeof frameId === "undefined") {
       return;
     }
 
-    this.frameClient.notifyFrameId(tabId, frameId);
-    this.frameRepository.addFrameId(tabId, frameId);
+    await this.frameClient.notifyFrameId(tabId, frameId);
+    await this.frameRepository.addFrameId(tabId, frameId);
   }
 
-  private onFindPortDisconnect(port: chrome.runtime.Port) {
+  private async onFindPortDisconnect(port: chrome.runtime.Port) {
     const tabId = port.sender?.tab?.id;
     const frameId = port.sender?.frameId;
     if (typeof tabId === "undefined" || typeof frameId === "undefined") {
       return;
     }
 
-    this.frameRepository.removeFrameId(tabId, frameId);
+    await this.frameRepository.removeFrameId(tabId, frameId);
   }
 }

src/background/command/BufferCommand.ts
@@ -1,14 +1,14 @@
 import type Command from "./Command";
 import type { CommandContext } from "./Command";
 import type { Completions } from "./Command";
-import type LastSelectedTab from "../tabs/LastSelectedTab";
+import type LastSelectedTabRepository from "../repositories/LastSelectedTabRepository";
 import BufferCommandHelper from "./BufferCommandHelper";
 
 class BufferCommand implements Command {
   constructor(
-    private readonly lastSelectedTab: LastSelectedTab,
+    private readonly lastSelectedTabRepository: LastSelectedTabRepository,
     private readonly bufferCommandHelper = new BufferCommandHelper(
-      lastSelectedTab
+      lastSelectedTabRepository
     )
   ) {}
 
@@ -56,7 +56,8 @@ class BufferCommand implements Command {
       return;
     } else if (keywords.trim() === "#") {
       // Select last selected window
-      const lastId = this.lastSelectedTab.get();
+      const lastId =
+        await this.lastSelectedTabRepository.getLastSelectedTabId();
       if (typeof lastId === "undefined" || lastId === null) {
         throw new Error("No last selected tab");
       }

src/background/command/BufferCommandHelper.ts
@@ -1,11 +1,14 @@
 import { Completions } from "../../shared/Completions";
-import LastSelectedTab from "../tabs/LastSelectedTab";
+import LastSelectedTabRepository from "../repositories/LastSelectedTabRepository";
 
 export default class BufferCommandHelper {
-  constructor(private readonly lastSelectedTab: LastSelectedTab) {}
+  constructor(
+    private readonly lastSelectedTabRepository: LastSelectedTabRepository
+  ) {}
 
   async getCompletions(force: boolean, query: string): Promise<Completions> {
-    const lastTabId = this.lastSelectedTab.get();
+    const lastTabId =
+      await this.lastSelectedTabRepository.getLastSelectedTabId();
     const allTabs = await this.getAllTabs(force);
     const num = parseInt(query, 10);
     let tabs: chrome.tabs.Tab[] = [];

src/background/command/index.ts
@@ -15,7 +15,7 @@ import PropertyRegistry from "../property/PropertyRegistry";
 import PropertySettings from "../settings/PropertySettings";
 import SearchEngineSettings from "../settings/SearchEngineSettings";
 import CommandRegistry, { CommandRegistryImpl } from "./CommandRegistry";
-import LastSelectedTab from "../tabs/LastSelectedTab";
+import LastSelectedTabRepository from "../repositories/LastSelectedTabRepository";
 import ConsoleClient from "../clients/ConsoleClient";
 
 @injectable()
@@ -33,11 +33,13 @@ export class CommandRegistryFactory {
     private readonly propertySettings: PropertySettings,
     @inject("SearchEngineSettings")
     private readonly searchEngineSettings: SearchEngineSettings,
-    @inject("LastSelectedTab")
-    private readonly lastSelectedTab: LastSelectedTab
+    @inject("LastSelectedTabRepository")
+    private readonly lastSelectedTabRepository: LastSelectedTabRepository
   ) {
     this.propertyRegistry = propertyRegistry;
-    this.bufferCommandHelper = new BufferCommandHelper(this.lastSelectedTab);
+    this.bufferCommandHelper = new BufferCommandHelper(
+      this.lastSelectedTabRepository
+    );
   }
 
   create(): CommandRegistry {
@@ -45,7 +47,10 @@ export class CommandRegistryFactory {
 
     registory.register(new AddBookmarkCommand(this.consoleClient));
     registory.register(
-      new BufferCommand(this.lastSelectedTab, this.bufferCommandHelper)
+      new BufferCommand(
+        this.lastSelectedTabRepository,
+        this.bufferCommandHelper
+      )
     );
     registory.register(new BufferDeleteCommand(this.bufferCommandHelper));
     registory.register(new BufferDeletesCommand(this.bufferCommandHelper));

src/background/db/LocalStorage.ts
@@ -1,0 +1,22 @@
+export default interface LocalCache<T> {
+  setValue(value: T): Promise<void>;
+
+  getValue(): Promise<T>;
+}
+
+export class LocalCacheImpl<T> {
+  constructor(private readonly key: string, private readonly initValue: T) {}
+
+  setValue(value: T): Promise<void> {
+    const data = JSON.stringify(value);
+    return chrome.storage.local.set({ [this.key]: data });
+  }
+
+  async getValue(): Promise<T> {
+    const kv = await chrome.storage.local.get(this.key);
+    if (this.key in kv) {
+      return JSON.parse(kv[this.key]);
+    }
+    return this.initValue;
+  }
+}

src/background/db/MemoryStorage.ts
@@ -1,21 +1,0 @@
-const db: { [key: string]: any } = {};
-
-export default class MemoryStorage<T> {
-  constructor(private readonly key: string, initValue: T) {
-    if (!(key in db)) {
-      this.set(initValue);
-    }
-  }
-
-  set(value: T): void {
-    const data = JSON.stringify(value);
-    if (typeof data === "undefined") {
-      throw new Error("value is not serializable");
-    }
-    db[this.key] = data;
-  }
-
-  get(): T {
-    return JSON.parse(db[this.key]);
-  }
-}

commit 2546c175efffba44136ff644923cb215c2f20d47

fix "unsupported operation" error on set command (#110)
Set command outputs an error "unsupported operation". This error is
caused by #96, which removed cached settings. An user can update
properties by "set" command and the updated values are stored in the
cached settings. This change reverts caches settings and names it
"TransientSettings". The loaded settings and updated properties by the
user are stored in it.

src/background/di.ts
@@ -21,7 +21,10 @@ import {
 } from "./repositories/ClipboardRepository";
 import { PropertySettingsImpl } from "./settings/PropertySettings";
 import { SearchEngineSettingsImpl } from "./settings/SearchEngineSettings";
-import { SettingsRepositoryImpl } from "./settings/SettingsRepository";
+import {
+  TransientSettingsRepository,
+  PermanentSettingsRepository,
+} from "./settings/SettingsRepository";
 import { KeyCaptureClientImpl } from "./clients/KeyCaptureClient";
 import { MarkRepositoryImpl } from "./repositories/MarkRepository";
 import { MarkModeRepositoryImpl } from "./repositories/MarkModeRepository";
@@ -71,7 +74,8 @@ container.bind("TopFrameClient").to(TopFrameClientImpl);
 container.bind("AddonEnabledRepository").to(AddonEnabledRepositoryImpl);
 container.bind("AddonEnabledClient").to(AddonEnabledClientImpl);
 container.bind("ToolbarPresenter").to(ToolbarPresenterImpl);
-container.bind("SettingsRepository").to(SettingsRepositoryImpl);
+container.bind("PermanentSettingsRepository").to(PermanentSettingsRepository);
+container.bind("SettingsRepository").to(TransientSettingsRepository);
 container
   .bind("PropertyRegistry")
   .toConstantValue(new PropertyRegistryFactry().create());

src/background/settings/SettingsRepository.ts
@@ -1,6 +1,8 @@
-import { injectable } from "inversify";
-import { defaultSettings, deserialize } from "../../settings";
+import { injectable, inject } from "inversify";
+import LocalCache, { LocalCacheImpl } from "../db/LocalStorage";
 import Settings from "../../shared/Settings";
+import { defaultSettings, serialize, deserialize } from "../../settings";
+import { SerializedSettings } from "../../settings/schema";
 
 type OnChangeListener = (value: Settings) => unknown;
 
@@ -13,7 +15,7 @@ export default interface SettingsRepository {
 }
 
 @injectable()
-export class SettingsRepositoryImpl implements SettingsRepository {
+export class PermanentSettingsRepository implements SettingsRepository {
   async load(): Promise<Settings> {
     const { settings } = await chrome.storage.sync.get("settings");
     if (!settings) {
@@ -29,7 +31,7 @@ export class SettingsRepositoryImpl implements SettingsRepository {
     }
   }
 
-  save(_value: Settings): Promise<void> {
+  async save(_value: Settings): Promise<void> {
     throw new Error("unsupported operation");
   }
 
@@ -54,5 +56,41 @@ export class SettingsRepositoryImpl implements SettingsRepository {
 
       f(settings);
     });
+  }
+}
+
+@injectable()
+export class TransientSettingsRepository implements SettingsRepository {
+  constructor(
+    @inject("PermanentSettingsRepository")
+    private readonly permanent: SettingsRepository,
+    private readonly cache: LocalCache<
+      SerializedSettings | undefined
+    > = new LocalCacheImpl(TransientSettingsRepository.name, undefined)
+  ) {
+    this.permanent.onChanged(this.sync.bind(this));
+  }
+
+  async load(): Promise<Settings> {
+    const cached = await this.cache.getValue();
+    if (typeof cached !== "undefined") {
+      return deserialize(cached);
+    }
+
+    const settings = await this.permanent.load();
+    await this.cache.setValue(serialize(settings));
+    return settings;
+  }
+
+  save(value: Settings): Promise<void> {
+    return this.cache.setValue(serialize(value));
+  }
+
+  onChanged(_f: OnChangeListener): void {
+    throw new Error("unsupported operation");
+  }
+
+  private async sync(newValue: Settings) {
+    await this.cache.setValue(serialize(newValue));
   }
 }

src/background/usecases/SettingsEventUseCase.ts
@@ -6,7 +6,7 @@ import EventUseCaseHelper from "./EventUseCaseHelper";
 @injectable()
 export default class SettingsUseCase {
   constructor(
-    @inject("SettingsRepository")
+    @inject("PermanentSettingsRepository")
     private readonly settingsRepository: SettingsRepository,
     @inject("ContentMessageClient")
     private readonly contentMessageClient: ContentMessageClient,

test/background/settings/SettingsRepository.test.ts
@@ -1,0 +1,139 @@
+import {
+  PermanentSettingsRepository,
+  TransientSettingsRepository,
+} from "../../../src/background/settings/SettingsRepository";
+import type Settings from "../../../src/shared/Settings";
+import MockLocalStorage from "../mock/MockLocalStorage";
+
+describe("PermanentSettingsRepository", () => {
+  const mockStorageGet = jest.spyOn(chrome.storage.sync, "get");
+
+  const sut = new PermanentSettingsRepository();
+
+  beforeEach(() => {
+    mockStorageGet.mockClear();
+  });
+
+  describe("#load", () => {
+    it("returns default settings", async () => {
+      mockStorageGet.mockResolvedValue({ settings: undefined });
+
+      const settings = await sut.load();
+      expect(settings.properties["complete"]).toBe("sbh");
+    });
+
+    it("returns current settings", async () => {
+      mockStorageGet.mockResolvedValue({
+        settings: {
+          properties: {
+            complete: "ssbbhh",
+          },
+        },
+      });
+
+      const settings = await sut.load();
+      expect(settings.properties["complete"]).toBe("ssbbhh");
+    });
+  });
+
+  describe("#save", () => {
+    it("throws an error", () => {
+      expect(sut.save({})).rejects.toThrowError();
+    });
+  });
+});
+
+class MockSettingsRepository {
+  private readonly listeners: Array<(settings: Settings) => unknown> = [];
+
+  save(): Promise<void> {
+    throw new Error("not implemented");
+  }
+
+  load(): Promise<Settings> {
+    throw new Error("not implemented");
+  }
+
+  onChanged(l: (settings: Settings) => unknown) {
+    this.listeners.push(l);
+  }
+
+  invalidate(settings: Settings) {
+    this.listeners.forEach((l) => l(settings));
+  }
+}
+
+describe("TransientSettingsRepository", () => {
+  const permanent = new MockSettingsRepository();
+  const mockPermanetLoad = jest.spyOn(permanent, "load");
+
+  mockPermanetLoad.mockResolvedValue({
+    properties: {
+      complete: "sbh",
+    },
+  });
+
+  beforeEach(() => {
+    mockPermanetLoad.mockClear();
+  });
+
+  describe("#load", () => {
+    const sut = new TransientSettingsRepository(
+      permanent,
+      new MockLocalStorage(undefined)
+    );
+
+    it("loads delegated value and caches it", async () => {
+      const settings1 = await sut.load();
+      expect(settings1.properties["complete"]).toBe("sbh");
+
+      const settings2 = await sut.load();
+      expect(settings2.properties["complete"]).toBe("sbh");
+
+      expect(mockPermanetLoad).toBeCalledTimes(1);
+    });
+  });
+
+  describe("#save", () => {
+    const sut = new TransientSettingsRepository(
+      permanent,
+      new MockLocalStorage(undefined)
+    );
+
+    it("saves to a permanent storage", async () => {
+      const settings1 = await sut.load();
+      expect(settings1.properties["complete"]).toBe("sbh");
+
+      await sut.save({ properties: { complete: "ssbbhh" } });
+
+      const settings2 = await sut.load();
+      expect(settings2.properties["complete"]).toBe("ssbbhh");
+    });
+  });
+
+  describe("#sync", () => {
+    const sut = new TransientSettingsRepository(
+      permanent,
+      new MockLocalStorage(undefined)
+    );
+
+    it("syncs to a permanent storage", async () => {
+      const settings1 = await sut.load();
+      expect(settings1.properties["complete"]).toBe("sbh");
+
+      await sut.save({ properties: { complete: "ssbbhh" } });
+
+      const settings2 = await sut.load();
+      expect(settings2.properties["complete"]).toBe("ssbbhh");
+
+      permanent.invalidate({
+        properties: {
+          complete: "hbs",
+        },
+      });
+
+      const settings3 = await sut.load();
+      expect(settings3.properties["complete"]).toBe("hbs");
+    });
+  });
+});

test/main.ts
@@ -40,4 +40,12 @@ global.chrome = {
       get: todo,
     },
   },
+  storage: {
+    sync: {
+      get: todo,
+      onChanged: {
+        addListener: todo,
+      },
+    },
+  },
 } as unknown as typeof chrome;