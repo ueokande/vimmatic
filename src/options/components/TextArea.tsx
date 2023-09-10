8d3529035199b8de57d0d324af0aaf79ec2bca1d
Shin'ya Ueoka <ueokande@i-beam.org>
2023-09-10 05:38:34 +0000

    wrap text area by scroll area container

--- src/options/components/TextArea.tsx
+++ src/options/components/TextArea.tsx
@@ -4,24 +4,29 @@ import Prism from "prismjs";
 import "prismjs/components/prism-json";
 import "prismjs/themes/prism-coy.css";
 
-const padding = 4;
-
 const Container = styled.div`
-  position: relative;
-  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  resize: both;
+  overflow: auto;
   border: ButtonBorder 1px solid;
   border-radius: 4px;
   &:focus-within {
     box-shadow: inset 0 0 3px AccentColor;
   }
-  padding: ${padding}px;
+`;
+
+const Content = styled.div`
+  margin: 8px;
+  position: relative;
+  overflow: hidden;
 `;
 
 const StyledPre = styled.pre`
   position: absolute;
   font-size: 14px;
-  width: calc(100% - ${padding}px * 2);
-  height: calc(100% - ${padding}px * 2);
+  width: 100%;
+  height: 100%;
   resize: none;
   margin: 0;
   padding: 0;
@@ -30,8 +35,8 @@ const StyledPre = styled.pre`
 const StyledTextarea = styled.textarea`
   position: absolute;
   font-size: 14px;
-  width: calc(100% - ${padding}px * 2);
-  height: calc(100% - ${padding}px * 2);
+  width: 100%;
+  height: 100%;
   padding: 0;
   margin: 0;
   outline: none;
@@ -47,6 +52,7 @@ type Props = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 const TextArea: React.FC<Props> = (props) => {
   const highlightContainer = React.useRef<HTMLDivElement>(null);
   const container = React.useRef<HTMLDivElement>(null);
+  const content = React.useRef<HTMLDivElement>(null);
   const textarea = React.useRef<HTMLTextAreaElement>(null);
   const [json, setJson] = React.useState(props.value as string);
 
@@ -75,26 +81,35 @@ const TextArea: React.FC<Props> = (props) => {
   }, []);
 
   const autoResize = React.useCallback(() => {
-    if (!textarea.current || !container.current) {
+    if (!textarea.current || !content.current) {
       return;
     }
 
-    const { scrollHeight } = textarea.current;
-    container.current.style.height = `${scrollHeight}px`;
+    // shrink scroll area to get the correct scroll area
+    content.current.style.width = "0";
+    content.current.style.height = "0";
+
+    const { scrollHeight, scrollWidth } = textarea.current;
+    console.log(scrollHeight, scrollWidth);
+    content.current.style.width = `${scrollWidth}px`;
+    content.current.style.height = `${scrollHeight}px`;
   }, []);
 
   return (
     <Container ref={container}>
-      <StyledPre>
-        <code ref={highlightContainer} />
-      </StyledPre>
-      <StyledTextarea
-        {...props}
-        ref={textarea}
-        onChange={onChange}
-        spellCheck={false}
-        value={json}
-      />
+      <Content ref={content}>
+        <StyledPre>
+          <code ref={highlightContainer} />
+        </StyledPre>
+        <StyledTextarea
+          {...props}
+          wrap="off"
+          ref={textarea}
+          onChange={onChange}
+          spellCheck={false}
+          value={json}
+        />
+      </Content>
     </Container>
   );
 };
