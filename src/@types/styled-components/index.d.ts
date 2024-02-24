9c5f39bba8a2bf26afcac00a0339791a3e43ab61
Shin'ya Ueoka <ueokande@i-beam.org>
2024-02-24 06:10:46 +0000

    remove styled-components

--- src/@types/styled-components/index.d.ts
+++
@@ -1,6 +0,0 @@
-import 'styled-components';
-import { ThemeProperties } from "../src/console/styles/theme";
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeProperties {}
-}
