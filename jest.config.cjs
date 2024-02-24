c23ffbd3ad71fd21801246d7125ad239a3054c01
Shin'ya Ueoka <ueokande@i-beam.org>
2024-02-24 08:44:47 +0000

    do not enable verbatimModuleSyntax for ts-jest

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  verbose: true,
+  testMatch: ["**/test/**/*.test.+(ts|tsx|js|jsx)"],
+  transform: {
+    // Use isolated tsconfig for jest to disable verbatimModuleSyntax
+    // https://github.com/kulshekhar/ts-jest/issues/4081
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
+  },
+  setupFiles: ["./test/main.ts"],
+};
