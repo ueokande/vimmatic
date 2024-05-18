f15326e31720b58177222122d7789d9ddd088aa6
Shin'ya Ueoka <ueokande@i-beam.org>
2024-05-18 08:17:44 +0000

    fix unexpected NavigationPresenter

--- test/content/presenters/NavigationPresenter.test.ts
+++ test/content/presenters/NavigationPresenter.test.ts
@@ -3,184 +3,56 @@
  */
 
 import { NavigationPresenterImpl } from "../../../src/content/presenters/NavigationPresenter";
-import { describe, beforeEach, it, expect } from "vitest";
+import { describe, beforeEach, test, expect } from "vitest";
 
 describe("NavigationPresenterImpl", () => {
-  let sut: NavigationPresenterImpl;
-
-  const testRel = (rel: string, html: string): Promise<void> => {
-    return new Promise((resolve) => {
-      const method =
-        rel === "prev"
-          ? sut.openLinkPrev.bind(sut)
-          : sut.openLinkNext.bind(sut);
-      document.body.innerHTML = html;
-      method();
-      setTimeout(() => {
-        expect(document.location.hash).toEqual(`#${rel}`);
-        resolve();
-      }, 0);
-    });
-  };
-  const testPrev = (html: string) => (): Promise<void> => testRel("prev", html);
-  const testNext = (html: string) => (): Promise<void> => testRel("next", html);
-
   beforeEach(() => {
-    sut = new NavigationPresenterImpl();
+    document.location.hash = "";
+    document.body.innerHTML = "";
   });
 
-  describe("#linkPrev", () => {
-    it(
-      'navigates to <link> elements whose rel attribute is "prev"',
-      testPrev('<link rel="prev" href="#prev" />'),
-    );
+  test.each([
+    '<link rel="prev" href="#prev" />',
+    '<link rel="prev bar" href="#prev" />',
+    '<link rel="foo prev" href="#prev" />',
+    '<link rel="foo prev bar" href="#prev" />',
+    '<a rel="prev" href="#prev">click me</a>',
+    '<a rel="prev bar" href="#prev">click me</a>',
+    '<a rel="foo prev" href="#prev">click me</a>',
+    '<a rel="foo prev bar" href="#prev">click me</a>',
+    '<a href="#dummy">preview</a><a href="#prev">go to prev</a>',
+    '<a href="#dummy">previously</a><a href="#prev">previous page</a>',
+    '<a href="#dummy">click me</a><a href="#prev">&lt;&lt;</a>',
+    `<a rel="prev" href="#dummy" onclick="return location = '#prev', false">go to prev</a>`,
+    '<a rel="prev" href="#dummy">click me</a><link rel="prev" href="#prev" />',
+    '<a href="#dummy">go to prev</a><a rel="prev" href="#prev">click me</a>',
+  ])("navigates to #prev for %s", async (html) => {
+    document.body.innerHTML = html;
+    new NavigationPresenterImpl().openLinkPrev();
 
-    it(
-      'navigates to <link> elements whose rel attribute starts with "prev"',
-      testPrev('<link rel="prev bar" href="#prev" />'),
-    );
-
-    it(
-      'navigates to <link> elements whose rel attribute ends with "prev"',
-      testPrev('<link rel="foo prev" href="#prev" />'),
-    );
-
-    it(
-      'navigates to <link> elements whose rel attribute contains "prev"',
-      testPrev('<link rel="foo prev bar" href="#prev" />'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute is "prev"',
-      testPrev('<a rel="prev" href="#prev">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute starts with "prev"',
-      testPrev('<a rel="prev bar" href="#prev">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute ends with "prev"',
-      testPrev('<a rel="foo prev" href="#prev">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute contains "prev"',
-      testPrev('<a rel="foo prev bar" href="#prev">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose text matches "prev"',
-      testPrev('<a href="#dummy">preview</a><a href="#prev">go to prev</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose text matches "previous"',
-      testPrev(
-        '<a href="#dummy">previously</a><a href="#prev">previous page</a>',
-      ),
-    );
-
-    it(
-      'navigates to <a> elements whose decoded text matches "<<"',
-      testPrev('<a href="#dummy">click me</a><a href="#prev">&lt;&lt;</a>'),
-    );
-
-    it(
-      "navigates to matching <a> elements by clicking",
-      testPrev(
-        `<a rel="prev" href="#dummy" onclick="return location = '#prev', false">go to prev</a>`,
-      ),
-    );
-
-    it(
-      "prefers link[rel~=prev] to a[rel~=prev]",
-      testPrev(
-        '<a rel="prev" href="#dummy">click me</a><link rel="prev" href="#prev" />',
-      ),
-    );
-
-    it(
-      "prefers a[rel~=prev] to a::text(pattern)",
-      testPrev(
-        '<a href="#dummy">go to prev</a><a rel="prev" href="#prev">click me</a>',
-      ),
-    );
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    expect(document.location.hash).toEqual("#prev");
   });
 
-  describe("#linkNext", () => {
-    it(
-      'navigates to <link> elements whose rel attribute is "next"',
-      testNext('<link rel="next" href="#next" />'),
-    );
+  test.each([
+    '<link rel="next" href="#next" />',
+    '<link rel="next bar" href="#next" />',
+    '<link rel="foo next" href="#next" />',
+    '<link rel="foo next bar" href="#next" />',
+    '<a rel="next" href="#next">click me</a>',
+    '<a rel="next bar" href="#next">click me</a>',
+    '<a rel="foo next" href="#next">click me</a>',
+    '<a rel="foo next bar" href="#next">click me</a>',
+    '<a href="#dummy">inextricable</a><a href="#next">go to next</a>',
+    '<a href="#dummy">click me</a><a href="#next">&gt;&gt;</a>',
+    `<a rel="next" href="#dummy" onclick="return location = '#next', false">go to next</a>`,
+    '<a rel="next" href="#dummy">click me</a><link rel="next" href="#next" />',
+    '<a href="#dummy">next page</a><a rel="next" href="#next">click me</a>',
+  ])("navigates to #next for %s", async (html) => {
+    document.body.innerHTML = html;
+    new NavigationPresenterImpl().openLinkNext();
 
-    it(
-      'navigates to <link> elements whose rel attribute starts with "next"',
-      testNext('<link rel="next bar" href="#next" />'),
-    );
-
-    it(
-      'navigates to <link> elements whose rel attribute ends with "next"',
-      testNext('<link rel="foo next" href="#next" />'),
-    );
-
-    it(
-      'navigates to <link> elements whose rel attribute contains "next"',
-      testNext('<link rel="foo next bar" href="#next" />'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute is "next"',
-      testNext('<a rel="next" href="#next">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute starts with "next"',
-      testNext('<a rel="next bar" href="#next">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute ends with "next"',
-      testNext('<a rel="foo next" href="#next">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose rel attribute contains "next"',
-      testNext('<a rel="foo next bar" href="#next">click me</a>'),
-    );
-
-    it(
-      'navigates to <a> elements whose text matches "next"',
-      testNext(
-        '<a href="#dummy">inextricable</a><a href="#next">go to next</a>',
-      ),
-    );
-
-    it(
-      'navigates to <a> elements whose decoded text matches ">>"',
-      testNext('<a href="#dummy">click me</a><a href="#next">&gt;&gt;</a>'),
-    );
-
-    it(
-      "navigates to matching <a> elements by clicking",
-      testNext(
-        `<a rel="next" href="#dummy" onclick="return location = '#next', false">go to next</a>`,
-      ),
-    );
-
-    it(
-      "prefers link[rel~=next] to a[rel~=next]",
-      testNext(
-        '<a rel="next" href="#dummy">click me</a><link rel="next" href="#next" />',
-      ),
-    );
-
-    it(
-      "prefers a[rel~=next] to a::text(pattern)",
-      testNext(
-        '<a href="#dummy">next page</a><a rel="next" href="#next">click me</a>',
-      ),
-    );
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    expect(document.location.hash).toEqual("#next");
   });
 });
