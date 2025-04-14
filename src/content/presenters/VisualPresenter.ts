a5cbd5cc923833ac25449dcdff13f15297f889a9
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2025-04-14 17:49:43 +0300

    feat(visual): add content visual presenter

--- src/content/presenters/VisualPresenter.ts
+++ src/content/presenters/VisualPresenter.ts
@@ -1,179 +1,308 @@
 import { inject } from "inversify";
 import { provide } from "inversify-binding-decorators";
 import { FinderRepository } from "../repositories/FinderRepository";
+import { SettingRepository } from "../repositories/SettingRepository";
 import type { FindRange } from "../repositories/FinderRepository";
 
 export interface VisualPresenter {
-    
-    select(): void
-
-    moveLeft(): void
-
-    moveRight(): void
-
-    moveNextWord(): void
-
-    moveEndWord(): void
-
-    movePrevWord(): void
-    
+  isValidCarret(): boolean;
+  updateCarrets(): void;
+  select(): void;
+  moveLeft(): void;
+  moveRight(): void;
+  moveNextWord(): void;
+  moveEndWord(): void;
+  movePrevWord(): void;
 }
-
 
 export const VisualPresenter = Symbol("VisualPresenter");
 
 @provide(VisualPresenter)
 export class VisualPresenterImpl implements VisualPresenter {
-  private currentCarretPosition: FindRange
-  private startOffset: number 
-  private endOffset: number
-  private startNode: Text
-  private endNode: Text
+  private initCarretPosition: FindRange;
+  private currentCarretPosition: FindRange;
+  private tempCarretPosition: FindRange;
+  private ignoreNodes: boolean | string | number;
 
   constructor(
     @inject(FinderRepository)
     private readonly finderRepository: FinderRepository,
+    @inject(SettingRepository)
+    private readonly settingRepository: SettingRepository,
   ) {
-    const currentCarretPosition = finderRepository.getCurrentCarretPosition();
-    this.currentCarretPosition = currentCarretPosition;
-    if (!currentCarretPosition) {
-        return
-    }
-    this.startOffset = currentCarretPosition[0].offset 
-    this.endOffset = currentCarretPosition[1].offset
-    this.startNode = currentCarretPosition[0].node
-    this.endNode = currentCarretPosition[1].node
+    this.ignoreNodes = this.settingRepository.getProperties()["ignorenodes"];
+    const mockFindRange: FindRange = [
+      {
+        node: document.createTextNode("start"),
+        offset: 0,
+        textNodePos: 0,
+      },
+      {
+        node: document.createTextNode("end"),
+        offset: 3,
+        textNodePos: 0,
+      },
+    ];
+    this.initCarretPosition = [
+      { ...mockFindRange[0] },
+      { ...mockFindRange[1] },
+    ];
+
+    this.currentCarretPosition = [
+      { ...mockFindRange[0] },
+      { ...mockFindRange[1] },
+    ];
+
+    this.tempCarretPosition = [
+      { ...mockFindRange[0] },
+      { ...mockFindRange[1] },
+    ];
+  }
+
+  updateCarrets(): void {
+    const currentCarretPosition = this.finderRepository.getCurrentMatch();
+
+    this.initCarretPosition = [
+      { ...currentCarretPosition[0] },
+      { ...currentCarretPosition[1] },
+    ];
+
+    this.currentCarretPosition = [
+      { ...currentCarretPosition[0] },
+      { ...currentCarretPosition[1] },
+    ];
+
+    this.tempCarretPosition = [
+      { ...currentCarretPosition[0] },
+      { ...currentCarretPosition[1] },
+    ];
+  }
+
+  isValidCarret(): boolean {
+    const currentMatch = JSON.stringify(this.initCarretPosition);
+    const newMatch = JSON.stringify(this.finderRepository.getCurrentMatch());
+
+    return !!this.currentCarretPosition && currentMatch === newMatch;
   }
 
   select(): void {
-      this.adjustRange();
-      const range: FindRange = [ 
-          {node: this.startNode, offset: this.startOffset},
-          {node: this.endNode, offset: this.endOffset},
-      ]
-      this.finderRepository.select(range);
+    this.adjustRange();
+    this.finderRepository.select(this.tempCarretPosition);
   }
 
-  getTextNodeIndex(node: Text): number {
-    if (!node.parentNode) return -1;
-        const textNodes = Array.from(node.parentNode.childNodes).filter(n => n.nodeType === Node.TEXT_NODE);
-        return textNodes.indexOf(node);
-    }
-  getText(): string { 
-      const initCarrentPosition = this.finderRepository.getCurrentMatch(); 
-      if (this.getTextNodeIndex(this.startNode) >= this.getTextNodeIndex(initCarrentPosition[0].node)) {
-          return this.endNode.data
-      } else {
-          return this.startNode.data
-      }
+  private adjustRange(): void {
+    const compare = (
+      a: { textNodePos: number; offset: number },
+      b: { textNodePos: number; offset: number },
+    ): number => {
+      return a.textNodePos !== b.textNodePos
+        ? a.textNodePos - b.textNodePos
+        : a.offset - b.offset;
+    };
+
+    const init = this.initCarretPosition[0];
+    const temp = this.tempCarretPosition[1];
+    const nodes = getNodes();
+
+    const [start, end] = compare(temp, init) < 0 ? [temp, init] : [init, temp];
+
+    this.currentCarretPosition = [
+      {
+        node: nodes[start.textNodePos],
+        offset: start.offset,
+        textNodePos: start.textNodePos,
+      },
+      {
+        node: nodes[end.textNodePos],
+        offset: end.offset,
+        textNodePos: end.textNodePos,
+      },
+    ];
   }
-  
-  isWordChar(char: string): boolean {
-        return ![" ", "\t", "\n", ".", ",", ";", "!", "?", ":", "(", ")", "[", "]", "{", "}", "-", "_", "\"", "'"].includes(char);
-    }
 
-   isBorder(position: number, borderSize: number) {
-      return (position > 0) && (position < borderSize)
-   }
+  private getText(): string {
+    return this.tempCarretPosition[1].node.data;
+  }
 
-  adjustRange(): void {
-    const initCarrentPosition = this.finderRepository.getCurrentMatch();
-    const currentStartNodePos = this.getTextNodeIndex(this.currentCarretPosition[0].node);
-    const currentEndNodePos = this.getTextNodeIndex(this.currentCarretPosition[1].node);
-    const initStartNodePos = this.getTextNodeIndex(initCarrentPosition[0].node);
-    if (
-      this.endOffset <= initCarrentPosition[0].offset && 
-      currentStartNodePos >= currentEndNodePos
-    ) {
-      [this.currentCarretPosition[0].node, this.currentCarretPosition[1].node] = [this.currentCarretPosition[1].node, this.currentCarretPosition[0].node];
-      this.currentCarretPosition[1].offset = this.endOffset;
-      [this.startOffset, this.endOffset] = [this.endOffset, initCarrentPosition[0].offset];
-    } else if (currentStartNodePos < initStartNodePos) { 
-      this.currentCarretPosition[1].offset = this.endOffset;
-      [this.startOffset, this.endOffset] = [this.endOffset, initCarrentPosition[0].offset];
-    } else { 
-      this.currentCarretPosition[1].offset = this.endOffset;
-      this.currentCarretPosition[0].offset = this.startOffset;
-    }
+  private isWordChar(char: string): boolean {
+    return (
+      !!char &&
+      ![
+        " ",
+        "\t",
+        "\n",
+        ".",
+        ",",
+        ";",
+        "!",
+        "?",
+        ":",
+        "(",
+        ")",
+        "[",
+        "]",
+        "{",
+        "}",
+        "-",
+        "_",
+        '"',
+        "'",
+      ].includes(char)
+    );
+  }
 
-    if (this.endOffset > this.endNode.length) {
-      this.endNode = this.endNode.nextSibling;
-      this.endOffset = 0;
-      this.currentCarretPosition[1].offset = this.endOffset;
-    } else if (this.endOffset < 0) {
-      this.endNode = this.endNode.previousSibling;
-      this.endOffset = this.endNode.data.length;
-      this.currentCarretPosition[1].offset = this.endOffset;
-    } else if (this.startOffset <= 0) {
-      this.endNode = this.startNode;
-      this.startNode = this.startNode.previousSibling;
-      this.startOffset = this.startNode.data.length;
-      this.endOffset = initCarrentPosition[0].offset;
-      this.currentCarretPosition[0].offset = this.endOffset;
-      this.currentCarretPosition[1].offset = this.startOffset;
-    } else if (this.startOffset > this.startNode.length) {
-      this.startNode = this.startNode.nextSibling;
-      this.startOffset = 0;
-      this.endOffset = initCarrentPosition[0].offset;
-      this.currentCarretPosition[1].offset = this.startOffset;
+  private isNotBorder(position: number, borderSize: number): boolean {
+    return this.ignoreNodes ? true : position >= 0 && position < borderSize;
+  }
+
+  private moveCarretLeft(): void {
+    if (this.tempCarretPosition[1].offset > 0) {
+      this.tempCarretPosition[1].offset--;
+    } else if (this.tempCarretPosition[1].textNodePos > 0) {
+      this.tempCarretPosition[1].textNodePos--;
+      const node = getNodes()[this.tempCarretPosition[1].textNodePos];
+      this.tempCarretPosition[1].node = node;
+      this.tempCarretPosition[1].offset = node.data.length;
     }
+  }
+
+  private moveCarretRight(): void {
+    const nodes = getNodes();
+    const node = this.tempCarretPosition[1].node;
+
+    if (this.tempCarretPosition[1].offset < node.data.length) {
+      this.tempCarretPosition[1].offset++;
+    } else if (this.tempCarretPosition[1].textNodePos < nodes.length - 1) {
+      this.tempCarretPosition[1].textNodePos++;
+      const nextNode = nodes[this.tempCarretPosition[1].textNodePos];
+      this.tempCarretPosition[1].node = nextNode;
+      this.tempCarretPosition[1].offset = 0;
+    }
+  }
+
+  moveLeft(): void {
+    this.moveCarretLeft();
+  }
+
+  moveRight(): void {
+    this.moveCarretRight();
   }
 
   moveNextWord(): void {
-     const text = this.getText();
-     let makeStep: boolean = true;
-     while (this.isWordChar(text[this.endOffset]) && this.isBorder(this.endOffset, text.length)) {
-          this.endOffset++;
-          makeStep = false;
-        }
-     while (!this.isWordChar(text[this.endOffset]) && this.isBorder(this.endOffset, text.length)) {
-          this.endOffset++;
-          makeStep = false;
-     }
-     if (makeStep) { this.endOffset++; }
- 
+    let text = this.getText();
+    let makeStep = true;
+
+    while (
+      this.isWordChar(text[this.tempCarretPosition[1].offset]) &&
+      this.isNotBorder(this.tempCarretPosition[1].offset, text.length)
+    ) {
+      if (!text[this.tempCarretPosition[1].offset]) {
+        text = this.getText();
+      }
+      this.moveCarretRight();
+      makeStep = false;
+    }
+
+    while (
+      !this.isWordChar(text[this.tempCarretPosition[1].offset]) &&
+      this.isNotBorder(this.tempCarretPosition[1].offset, text.length)
+    ) {
+      if (!text[this.tempCarretPosition[1].offset]) {
+        text = this.getText();
+      }
+      this.moveCarretRight();
+      makeStep = false;
+    }
+
+    if (makeStep) {
+      this.moveCarretRight();
+    }
   }
 
   moveEndWord(): void {
-     const text = this.getText();
-     let makeStep: boolean = true;
-     while (!this.isWordChar(text[this.endOffset]) && this.isBorder(this.endOffset, text.length)) {
-          this.endOffset++;
-          makeStep = false;
+    let text = this.getText();
+    let makeStep = true;
+
+    if (this.isWordChar(text[this.tempCarretPosition[1].offset])) {
+      while (
+        this.isWordChar(text[this.tempCarretPosition[1].offset]) &&
+        this.isNotBorder(this.tempCarretPosition[1].offset, text.length)
+      ) {
+        if (!text[this.tempCarretPosition[1].offset]) {
+          text = this.getText();
         }
-     while (this.isWordChar(text[this.endOffset]) && this.isBorder(this.endOffset, text.length)) {
-          this.endOffset++;
-          makeStep = false;
-     }
-     if (makeStep) { this.endOffset++; }
-      
+        makeStep = false;
+        this.moveCarretRight();
+      }
+    } else {
+      while (
+        !this.isWordChar(text[this.tempCarretPosition[1].offset]) &&
+        this.isNotBorder(this.tempCarretPosition[1].offset, text.length)
+      ) {
+        if (!text[this.tempCarretPosition[1].offset]) {
+          text = this.getText();
+        }
+        makeStep = false;
+        this.moveCarretRight();
+      }
+
+      while (
+        this.isWordChar(text[this.tempCarretPosition[1].offset]) &&
+        this.isNotBorder(this.tempCarretPosition[1].offset, text.length)
+      ) {
+        if (!text[this.tempCarretPosition[1].offset]) {
+          text = this.getText();
+        }
+        makeStep = false;
+        this.moveCarretRight();
+      }
+    }
+
+    if (makeStep) {
+      this.moveCarretRight();
+    }
   }
 
   movePrevWord(): void {
-     const text = this.getText();
-     let makeStep: boolean = true;
-     while (!this.isWordChar(text[this.endOffset]) && this.isBorder(this.endOffset, text.length)) {
-          this.endOffset--;
-          makeStep = false;
-        }
-     while (this.isWordChar(text[this.endOffset]) && this.isBorder(this.endOffset, text.length)) {
-          this.endOffset--;
-          makeStep = false;
-     }
-     if (makeStep) { this.endOffset++; }
-      
+    let text = this.getText();
+    let makeStep = true;
+
+    while (
+      !this.isWordChar(text[this.tempCarretPosition[1].offset]) &&
+      this.isNotBorder(this.tempCarretPosition[1].offset, text.length)
+    ) {
+      if (!text[this.tempCarretPosition[1].offset]) {
+        text = this.getText();
+      }
+      this.moveCarretLeft();
+      makeStep = false;
+    }
+
+    while (
+      this.isWordChar(text[this.tempCarretPosition[1].offset]) &&
+      this.isNotBorder(this.tempCarretPosition[1].offset, text.length)
+    ) {
+      if (!text[this.tempCarretPosition[1].offset]) {
+        text = this.getText();
+      }
+      this.moveCarretLeft();
+      makeStep = false;
+    }
+
+    if (makeStep) {
+      this.moveCarretLeft();
+    }
   }
-
- 
-  moveRight() {
-      this.endOffset++;
-  }
-
-  moveLeft() {
-      this.endOffset--;
-  }
-
-
 }
 
+function getNodes(): Text[] {
+  const walker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
+  const nodes: Text[] = [];
+  let node: Node | null;
 
+  while ((node = walker.nextNode())) {
+    nodes.push(node as Text);
+  }
+
+  return nodes;
+}
