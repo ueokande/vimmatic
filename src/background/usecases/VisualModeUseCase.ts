96c9afc0774e1adb706735dc2a7a5b5770520cd8
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2025-04-14 15:18:42 +0300

    feat(visual): add background visual mode use cases

--- src/background/usecases/VisualModeUseCase.ts
+++ src/background/usecases/VisualModeUseCase.ts
@@ -1,29 +1,25 @@
-import {injectable, inject } from "inversify";
+import { injectable, inject } from "inversify";
 import { ReadyFrameRepository } from "../repositories/ReadyFrameRepository";
-import { PropertySettings } from "../settings/PropertySettings";
-import { TopFrameClient } from "../clients/TopFrameClient";
-
-
 
 @injectable()
 export class VisualModeUseCase {
-    constructor(
-        @inject(TopFrameClient)
-        private readonly topFrameClient: TopFrameClient,
-        @inject(ReadyFrameRepository)
-        private readonly frameRepository: ReadyFrameRepository,
-        @inject(PropertySettings)
-        private readonly propertySettings: PropertySettings,
-    ) {}
-    
+  constructor(
+    @inject(ReadyFrameRepository)
+    private readonly frameRepository: ReadyFrameRepository,
+  ) {}
 
-  async start(
-      tabId: number, 
-      visualModeName: string 
-    ): Promise<void> {
+  pressKey(key: string): boolean {
+    switch (key) {
+      case "Esc":
+        return false;
+    }
+    return true;
+  }
+
+  async start(tabId: number): Promise<void> {
     const frameIds = await this.frameRepository.getFrameIds(tabId);
     if (typeof frameIds === "undefined") {
-        return;
-      }
+      return;
     }
+  }
 }
