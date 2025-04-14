f9a1e78ef05e0e5207adf3e95120ccbab2fd0f79
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2025-04-14 15:08:44 +0300

    feat(visual): add background visual operators

--- src/background/operators/impls/StartVisualOperator.ts
+++ src/background/operators/impls/StartVisualOperator.ts
@@ -6,27 +6,21 @@ import { Mode } from "../../../shared/mode";
 
 @injectable()
 export class StartVisualOperator implements Operator {
-    constructor(
-        @inject(VisualModeUseCase)
-        private readonly visualModeUseCase: VisualModeUseCase,
-        @inject(ModeUseCase)
-        private readonly modeUseCase: ModeUseCase,
-    ) {}
+  constructor(
+    @inject(VisualModeUseCase)
+    private readonly visualModeUseCase: VisualModeUseCase,
+    @inject(ModeUseCase)
+    private readonly modeUseCase: ModeUseCase,
+  ) {}
 
   name(): string {
-      return "find.visual.start";
+    return "find.visual.start";
   }
 
   schema() {}
 
-  async run(
-      ctx: OperatorContext,
-  ): Promise<void> {
-      await this.visualModeUseCase.start(
-          ctx.sender.tabId,
-          "find.visual.start",
-    );
+  async run(ctx: OperatorContext): Promise<void> {
+    await this.visualModeUseCase.start(ctx.sender.tabId, "find.visual.start");
     await this.modeUseCase.setMode(ctx.sender.tabId, Mode.Visual);
   }
- 
 }
